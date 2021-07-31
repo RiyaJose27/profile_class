5a8589cb2ad939ed8b3a90bf5d67cf42f029f8f8
Riya Jose Pratheeksh <rjosepratheeksh@gmail.com>
2021-07-31 17:44:07 -0500

    Profile Javascript

---
+++ prof.js
@@ -0,0 +1,29 @@
+var proCount= document.querySelector("#nam")
+
+
+function addTo(e){
+proCount.innerText = "Riya Jose";
+}
+
+
+var seeCount= document.getElementById("pic");
+
+function hello() {
+    seeCount.remove();
+
+}
+
+// function heo() {
+//     var elemnt = document.getElementById('he')
+//     var value = element.innerHTML;
+//     ++value;
+//     console.log(value);
+//     document.getElementById('he').innerHTML = value;
+// }
+
+var trueCount = document.querySelector("#he")
+var items = 2;
+function heo() {
+    items ++;
+    trueCount.innerText = items;
+}
